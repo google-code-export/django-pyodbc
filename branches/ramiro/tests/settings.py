a55c221566708bbb69ad98d7e2249c0603eeb1c9
cramm0 <cramm0@d8ed95fc-f742-0410-923b-f1a0a12816d3>
2009-01-16 10:04:24 +0000

    ramiro branch: Added aggregation{,-regress} tests stolen from the Django test suite.

--- branches/ramiro/tests/settings.py
+++ branches/ramiro/tests/settings.py
@@ -76,6 +76,10 @@ INSTALLED_APPS = (
     #'django.contrib.sessions',
     #'django.contrib.sites',
 
+    # Include this app to get Django 'loaddata' management command
+    # automatically overriden by our 'ss_loaddata' one. This allows
+    # the loading of fixtures with forward references and is tested
+    # by the 'fixtures' test below.
     'sql_server.extra',
 
     'issue019',
@@ -84,4 +88,6 @@ INSTALLED_APPS = (
     'basic',
     'order_with_respect_to',
     'fixtures',
+    'aggregation',
+    'aggregation_regress',
 )
